bc490e44528a049b35c691ffd304656f2c420160
Arshh26 <arshadali99936@gmail.com>
2025-08-25 21:45:30 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import MyButton from "./MyButton";
+
+//Functional Component
+function App() {
+  return <div>
+    <h1>
+    Hello World!
+    </h1>
+    <MyButton></MyButton>
+    
+</div>
+}
+
+export default App; 
+/* it is a React (actually JavaScript ES6) feature used for exporting a component (or any value) from a file so you can import it somewhere else.*/
+
