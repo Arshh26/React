3ef6def01e6675479e295b319883941fb113f05d
Arshh26 <arshadali99936@gmail.com>
2025-08-26 22:09:17 +0530

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import MyButton from "./MyButton";
 import Hello from "./Hello";
 import Random from"./Random";
@@ -25,3 +26,40 @@ export default App;
 /* it is a React (actually JavaScript ES6) feature used for exporting a component (or any value) from a file so you can import it somewhere else.*/
 /*ES6: ES6 means ECMAScript 6, also called ECMAScript 2015.
 It’s just a new version of JavaScript that added many cool and modern features to make coding easier.*/
+=======
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
+>>>>>>> 6f0e698 (Initial commit)
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+<<<<<<< HEAD
+=======
+import 'bootstrap/dist/css/bootstrap.min.css'
+>>>>>>> 6f0e698 (Initial commit)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
