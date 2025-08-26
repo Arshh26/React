7c6845ff2f0d518db7356528af299a82ba76dcdd
Arshh26 <arshadali99936@gmail.com>
2025-08-26 21:57:39 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import MyButton from "./MyButton";
+import Hello from "./Hello";
+import Random from"./Random";
+
+
+/*import the default exported component MyButton from MyButton.jsx file*/
 
 //Functional Component
 function App() {
@@ -7,10 +12,16 @@ function App() {
     Hello World!
     </h1>
     <MyButton></MyButton>
-    
+    <Hello></Hello>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
+    <Random></Random>
 </div>
 }
 
 export default App; 
-/* it is a React (actually JavaScript ES6) feature used for exporting a component (or any value) from a file so you can import it somewhere else.*/
 
+/* it is a React (actually JavaScript ES6) feature used for exporting a component (or any value) from a file so you can import it somewhere else.*/
+/*ES6: ES6 means ECMAScript 6, also called ECMAScript 2015.
+It’s just a new version of JavaScript that added many cool and modern features to make coding easier.*/
